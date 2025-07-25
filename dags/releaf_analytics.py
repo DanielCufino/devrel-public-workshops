b0bbea67145ed80f1fc673307520525e2214d3df
TJaniF <tamara.fingerlin@astronomer.io>
2025-07-25 10:58:16 +0200

    workshop version

--- dags/releaf_analytics.py
+++ dags/releaf_analytics.py
@@ -12,16 +12,4 @@ _DUCKDB_INSTANCE_NAME = os.getenv("DUCKDB_INSTANCE_NAME", f"{_INCLUDE_PATH}/rele
 
 
 @asset(schedule=None)
-def releaf_analytics():
-
-    sql_file_path = f"{_INCLUDE_PATH}/sql/releaf_analytics.sql"
-
-    with open(sql_file_path, "r") as file:
-        analytics_query = file.read()
-
-    cursor = duckdb.connect(_DUCKDB_INSTANCE_NAME)
-    results = cursor.execute(analytics_query).fetchall()
-    cursor.close()
-
-    for result in results:
-        print(result)
+# Add your function here
